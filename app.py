92c35cf5736f360112c58fbcb57bf4032cd078ec
Jingjing Duan <duan0029@algonquinlive.com>
2025-06-23 16:28:15 -0400

    port

--- app.py
+++ app.py
@@ -209,4 +209,5 @@ def download_template():
     return send_from_directory(directory='downloads', path='presidents.csv', as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
